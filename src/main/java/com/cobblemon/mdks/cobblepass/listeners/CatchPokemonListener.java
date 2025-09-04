a0d93fcd48bc7b51a8acc48f0f4596a160254870
Bloo <55114278+9iy@users.noreply.github.com>
2025-09-03 23:05:12 -0700

    Update CatchPokemonListener.java for 1.7
    same as other comment

--- src/main/java/com/cobblemon/mdks/cobblepass/listeners/CatchPokemonListener.java
+++ src/main/java/com/cobblemon/mdks/cobblepass/listeners/CatchPokemonListener.java
@@ -1,31 +1,51 @@
 package com.cobblemon.mdks.cobblepass.listeners;
 
 import com.cobblemon.mdks.cobblepass.CobblePass;
-import com.cobblemon.mdks.cobblepass.battlepass.PlayerBattlePass;
-import com.cobblemon.mdks.cobblepass.config.Config;
-import com.cobblemon.mdks.cobblepass.util.Logger;
 import com.cobblemon.mod.common.api.Priority;
-import com.cobblemon.mod.common.api.events.CobblemonEvents;
-import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent;
 import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 import net.minecraft.server.level.ServerPlayer;
 
 public class CatchPokemonListener {
-    private static final Logger LOGGER = new Logger("CobblePass");
 
     public static void register() {
-        CobblemonEvents.POKEMON_CAPTURED.subscribe(Priority.NORMAL, CatchPokemonListener::handle);
+        Function1<Object, Unit> handler = (evt) -> {
+            ServerPlayer player = tryGetPlayer(evt);
+            if (player != null) {
+                CobblePass.battlePass.addXP(player, CobblePass.config.getCatchXP());
+            }
+            return Unit.INSTANCE;
+        };
+        
+        if (trySubscribe("com.cobblemon.mod.common.api.events.pokemon.PokemonEvents", "CAPTURED", handler)) {
+            CobblePass.LOGGER.info("[CobblePass] Subscribed to PokemonEvents.CAPTURED");
+            return;
+        }
+        if (trySubscribe("com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent", "TYPE", handler)) {
+            CobblePass.LOGGER.info("[CobblePass] Subscribed to PokemonCapturedEvent.TYPE");
+            return;
+        }
+        if (trySubscribe("com.cobblemon.mod.common.api.events.CobblemonEvents", "POKEMON_CAPTURED", handler)) {
+            CobblePass.LOGGER.info("[CobblePass] Subscribed to CobblemonEvents.POKEMON_CAPTURED");
+            return;
+        }
+        CobblePass.LOGGER.warn("[CobblePass] No capture event channel found; capture XP disabled.");
     }
 
-    private static Unit handle(PokemonCapturedEvent event) {
-        if (event.getPlayer() instanceof ServerPlayer player) {
-            PlayerBattlePass battlePass = CobblePass.battlePass.getPlayerPass(player);
-            if (battlePass != null) {
-                int xp = CobblePass.config.getCatchXP();
-                battlePass.addXP(xp);
-                LOGGER.debug("Awarded " + xp + " XP to " + player.getName().getString() + " for catching a Pokémon");
-            }
-        }
-        return Unit.INSTANCE;
+    private static boolean trySubscribe(String className, String fieldName, Function1<Object, Unit> handler) {
+        try {
+            Class<?> clazz = Class.forName(className);
+            Object channel = clazz.getField(fieldName).get(null);
+            channel.getClass().getMethod("subscribe", Priority.class, Function1.class)
+                    .invoke(channel, Priority.NORMAL, handler);
+            return true;
+        } catch (Throwable ignored) { return false; }
+    }
+
+    private static ServerPlayer tryGetPlayer(Object evt) {
+        try {
+            Object p = evt.getClass().getMethod("getPlayer").invoke(evt);
+            return (p instanceof ServerPlayer) ? (ServerPlayer) p : null;
+        } catch (Throwable ignored) { return null; }
     }
 }
